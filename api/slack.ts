e6e0d17e752f7a92d81b50b73b6a74708ffce8aa
Michael Hurhangee <m.hurhangee@me.com>
2025-05-04 18:50:17 +0000

    feat: add waituntil and basic assistant

--- api/slack.ts
+++ api/slack.ts
@@ -1,10 +1,13 @@
 // api/slack.ts
-import { SlackApp } from 'slack-edge'
+import { SlackApp, Assistant } from 'slack-edge'
+import { waitUntil } from '@vercel/functions';
 
+// Edge Runtime
 export const config = {
   runtime: 'edge'
 }
 
+// Set up Slack App
 const app = new SlackApp({
   env: {
     SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET!,
@@ -13,10 +16,28 @@ const app = new SlackApp({
   }
 })
 
+// Create Assistant
+app.assistant(
+  new Assistant({
+    threadStarted: async ({ context: { say, setSuggestedPrompts } }) => {
+      await say({ text: "Hi, how can I help you today?" });
+      await setSuggestedPrompts({
+        prompts: ["What does SLACK stand for?"],
+      });
+    },
+    userMessage: async ({ context: { setStatus, say } }) => {
+      await setStatus({ status: "is typing..." });
+      await say({ text: "Here you are!" });
+    },
+   }),
+);
+
+// Create Slash Command
 app.command('/moo-hello', async (req) => {
   return '🐮 Mooooooo from MooAI!'
 })
 
+// Create HTTP Handler
 export default async function handler(req: Request): Promise<Response> {
-  return await app.run(req)
+  return await app.run(req, { waitUntil })
 }
